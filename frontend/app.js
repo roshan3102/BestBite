fefe2207723ebd841cf4d44a45f4c2dc03209a33
roshan3102 <roshan3102@gmail.com>
2023-12-29 19:01:41 -0500

    rating system

--- frontend/app.js
+++ frontend/app.js
@@ -7,3 +7,32 @@ function search() {
     var resultsContainer = document.getElementById('results');
     resultsContainer.innerHTML = `<p>Searching for ${userInput}...</p>`;
 }
+function submitRatings() {
+    const tasteRating = document.getElementById('tasteRating').value;
+    const fillingRating = document.getElementById('fillingRating').value;
+
+    // Assuming you have a function to get the foodItemId dynamically
+    const foodItemId = getFoodItemId();
+
+    // Make a POST request to your backend to update ratings
+    fetch('/api/rateFoodItem', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            foodItemId,
+            tasteRating,
+            fillingRating,
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Handle success, e.g., show a confirmation message
+        console.log(data);
+    })
+    .catch(error => {
+        // Handle error, e.g., show an error message
+        console.error('Error:', error);
+    });
+}
